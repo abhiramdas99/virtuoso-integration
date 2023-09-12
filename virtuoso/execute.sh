ed7c893ba2c65ecb4df41bf0121cbabdd9029610
Abhi <abhiramdas99@gmail.com>
2023-09-12 12:25:59 +0530

    Update execute.sh

--- virtuoso/execute.sh
+++ virtuoso/execute.sh
@@ -2,4 +2,10 @@
 
 #Exit from the script immideatly  on failure of any command 
 set -eou pipefail
-echo "welcome"
+
+#Exit from the script, if you have not passing any parameter 
+if [ $# -eq 0 ]; then
+    echo "Usage: ./execute.sh -t VIRTUOSO_TOKEN --goal_id ID_OF_GOAL_TO_EXECUTE [--staging] [--app2] [--max_retry_time MAX_RETRY_TIME] [--retry_delay_time RETRY_DELAY_TIME]"
+    exit 1
+fi
+
